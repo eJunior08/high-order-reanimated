b4845d54c842d5948b6ae4c387bdfb62110b2f00
Elson Oliveira <elsonfojr@gmail.com>
2021-08-14 00:26:05 -0300

    Animation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import {
+  withTiming,
+  withRepeat,
+  useSharedValue,
+  Easing,
+} from "react-native-reanimated";
+import { withPause } from "react-native-redash";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { Button, StyleGuide } from "./src/components/core";
 
+import ChatBubble from "./src/components/ChatBubble";
+
+const easing = Easing.inOut(Easing.ease);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "space-between",
+    backgroundColor: StyleGuide.palette.background,
   },
 });
+
+const App = () => {
+  const [play, setPlay] = useState(false);
+  const progress = useSharedValue<null | number>(null);
+  const paused = useSharedValue(!play);
+
+  return (
+    <View style={styles.container}>
+      <ChatBubble progress={progress} />
+      <Button
+        label={play ? "Pause" : "Play"}
+        primary
+        onPress={() => {
+          setPlay((prev) => !prev);
+          paused.value = !paused.value;
+          if (progress.value === null) {
+            progress.value = withPause(
+              withRepeat(
+                withTiming(1, {
+                  duration: 1000,
+                  easing: Easing.inOut(Easing.ease),
+                }),
+                -1,
+                true
+              ),
+              paused
+            );
+          }
+        }}
+      />
+    </View>
+  );
+};
+
+export default App;
